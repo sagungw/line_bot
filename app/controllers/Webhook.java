bf4af88ac1b08f2e80f7a8a5e0eb3c004dc17262
Huteri Manza <hut3ri@gmail.com>
2017-03-29 00:43:26 +0700

    fix label with more than 20chars

--- app/controllers/Webhook.java
+++ app/controllers/Webhook.java
@@ -35,6 +35,8 @@ import java.util.stream.Collectors;
 
 public class Webhook extends Controller {
 
+    private final static String NON_THIN = "[^iIl1\\.,']";
+
     private String lineChannelToken;
 
     private TheaterMovieRepository theaterMovieRepository;
@@ -69,7 +71,16 @@ public class Webhook extends Controller {
                 responseMessage = new TextMessage("Sorry, I don't know where that theater is.");
             } else {
                 if (theaters.size() > 1) {
-                    List<Action> actions = theaters.stream().map(t -> new MessageAction(t.getName(), t.getName())).collect(Collectors.toList());
+                    List<Action> actions = theaters.stream().map(t -> {
+                        String name = t.getName();
+                        if(name.length() > 20) {
+                           name = ellipsize(name, 20);
+                        }
+
+                        MessageAction messageAction = new MessageAction(name, t.getName());
+
+                        return messageAction;
+                    }).collect(Collectors.toList());
 
                     ButtonsTemplate template = new ButtonsTemplate( null, "Options", "More than one theaters found. Choose one.", actions);
                     responseMessage = new TemplateMessage("alttext", template);
@@ -110,4 +121,35 @@ public class Webhook extends Controller {
         return ok("nothing");
     }
 
+    public static String ellipsize(String text, int max) {
+
+        if (textWidth(text) <= max)
+            return text;
+
+        // Start by chopping off at the word before max
+        // This is an over-approximation due to thin-characters...
+        int end = text.lastIndexOf(' ', max - 3);
+
+        // Just one long word. Chop it off.
+        if (end == -1)
+            return text.substring(0, max-3) + "...";
+
+        // Step forward as long as textWidth allows.
+        int newEnd = end;
+        do {
+            end = newEnd;
+            newEnd = text.indexOf(' ', end + 1);
+
+            // No more spaces.
+            if (newEnd == -1)
+                newEnd = text.length();
+
+        } while (textWidth(text.substring(0, newEnd) + "...") < max);
+
+        return text.substring(0, end) + "...";
+    }
+
+    private static int textWidth(String str) {
+        return (int) (str.length() - str.replaceAll(NON_THIN, "").length() / 2);
+    }
 }
